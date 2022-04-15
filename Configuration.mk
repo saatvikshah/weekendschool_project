7fb741e6cd2cda19f2f9742cbe55f04e9d928405
Antony Savio <antonysavio93@yahoo.co.in>
2022-04-15 20:35:47 +0200

    Update configuration for Antony's laptop.

--- Configuration.mk
+++ Configuration.mk
@@ -1,5 +1,4 @@
-MONITOR_PORT  = /dev/cu.usbserial-1430
-ARDUINO_DIR   = /Applications/Arduino.app/Contents/Java
-ARDMK_DIR     = /usr/local/opt/arduino-mk/
+ARDUINO_DIR   = /home/frozenjesuscola/Desktop/arduino-1.8.19
+ARDMK_DIR     = /usr/share/arduino/
 ARDUINO_LIBS = Cytron_Maker_Sumo_Library EEPROM Servo
 BOARD_TAG    = uno
